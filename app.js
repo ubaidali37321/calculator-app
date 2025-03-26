3e442eb02a9a627e3484d34b109a12e98597ea17
ubaid <ubaidali37321@gmail.com>
2025-03-26 23:02:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let input = document.querySelector('input');
+
+let getValue = (e) =>{
+    let val = e.target.innerText
+    input.value += val;
+}
+
+let result = () =>{
+    let res = eval(input.value)
+    input.value = res
+}
+
+let empty = () =>{
+    input.value = ""
+}
